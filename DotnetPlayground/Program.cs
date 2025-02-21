d2ce97961bfd5b7c14cca73649eebb44f6d0e147
siujamo <jamo.siu@outlook.com>
2025-02-21 16:58:37 +0800

    feat: 初始提交

---
+++ DotnetPlayground/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotnetPlayground.Util;
+
+var sqls = new List<string>();
+for (var year = 2010; year <= 2030; year++)
+{
+    for (var month = 1; month <= 12; month++)
+    {
+        var sql = $"select * from {TableNameComposer.Compose("gec_project_cert_info", year, month)}";
+        sqls.Add(sql);
+    }
+}
+
+Console.WriteLine(string.Join("\nunion all\n", sqls));
---
+++ DotnetPlayground/Util/TableNameComposer.cs
@@ -0,0 +1,16 @@
+﻿namespace DotnetPlayground.Util;
+
+public static class TableNameComposer
+{
+    /// <summary>
+    /// 根据给定的前缀、年份和月份生成表名。
+    /// </summary>
+    /// <param name="prefix">表名前缀</param>
+    /// <param name="year">年份</param>
+    /// <param name="month">月份</param>
+    /// <returns>格式化后的表名，形如 "prefix_year_month"。</returns>
+    public static string Compose(string prefix, int year, int month)
+    {
+        return $"{prefix}_{year}_{month:00}";
+    }
+}
